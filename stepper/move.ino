1edd547b5e8fa30362c1aabb5f4b2b2744571974
jboud030 <boudreau.j4@gmail.com>
2015-09-12 11:22:35 -0400

    arduino code

---
+++ stepper/move.ino
@@ -0,0 +1,53 @@
+void movepoint(double now[],int interval,AccelStepper motorx,AccelStepper motory,int x,int y){
+  int resolution=200*20/25.4; //step/mm
+  int newx=x-now[0];
+  int newy=y-now[1];
+  
+  //Serial.println(newx);
+  //Serial.println(newy);
+  
+  newx=newx*interval*resolution; //steps
+  newy=newy*interval*resolution; //steps
+  
+  motorx.move(newx); //steps
+  motory.move(newy);
+  long spd=300; //steps/sec
+  long xspd=spd;
+  long yspd=spd;
+  long check=millis();
+
+    if (newx==0 && newy==0){
+      xspd=0;
+      yspd=0;
+    }else if (newx==0 && newy!=0){
+      xspd=0;
+    }else if (newy==0 && newx!=0){
+      yspd=0;
+    }else if (abs(newx)>abs(newy) && newx!=0 && newy!=0){
+      yspd=abs(spd*newy/newx);
+    }else if (abs(newx)<abs(newy) && newx!=0 && newy!=0){
+      xspd=abs(spd*newx/newy); //step/sec
+    }
+    //if (newx<0) xspd=-xspd;
+    //if (newy<0) yspd=-yspd;
+
+  while (motorx.distanceToGo()!=0 || motory.distanceToGo()!=0){
+    
+    motorx.setSpeed(xspd);
+    motory.setSpeed(yspd);
+    motorx.runSpeedToPosition();
+    motory.runSpeedToPosition();
+    
+    if (millis()-check>=1000){
+      //Serial.print("X pos=");
+      //Serial.println(motorx.currentPosition());
+      //Serial.print("Y pos=");
+      //Serial.println(motory.currentPosition());
+      //delay(500);
+      check=millis();
+    }
+  }
+  now[0]=x;
+  now[1]=y;
+}
+
---
+++ stepper/stepper.ino
@@ -0,0 +1,48 @@
+#include <AccelStepper.h>
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // twelve servo objects can be created on most boards
+ 
+int pos = 90;    // variable to store the servo position
+double now[2]={0,0}; //x and y
+char input;
+int steps=200;
+int grid=20;
+long stsec=1000.0; //steps/sec
+int interval=110/grid; //mm
+AccelStepper Y(AccelStepper::FULL4WIRE,6,5,4,3);
+AccelStepper X(AccelStepper::FULL4WIRE,11,10,9,8);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  myservo.attach(13);  // attaches the servo on pin 13 to the servo object
+  myservo.write(pos);
+  while (!Serial);
+  Serial.println("Enter '1' to begin");
+  X.setMaxSpeed(stsec);
+  Y.setMaxSpeed(stsec);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (input!='1'){
+    input=Serial.read();
+  }
+  movepoint(now,interval,X,Y,3,0);
+  up_down(myservo); //down
+  movepoint(now,interval,X,Y,6,4);
+  movepoint(now,interval,X,Y,5,5);
+  movepoint(now,interval,X,Y,4,5);
+  movepoint(now,interval,X,Y,3,4);
+  movepoint(now,interval,X,Y,2,5);
+  movepoint(now,interval,X,Y,1,5);
+  movepoint(now,interval,X,Y,0,4);
+  movepoint(now,interval,X,Y,3,0);
+  up_down(myservo); //up
+  movepoint(now,interval,X,Y,0,0);
+  while (true);
+  
+}
