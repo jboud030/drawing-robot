20a1d7ca519ba66ff5317d92ea1612c3cfb2a59f
jboud030 <boudreau.j4@gmail.com>
2015-09-27 11:16:49 -0400

    Update Up_down.ino

--- stepper/Up_down.ino
+++ stepper/Up_down.ino
@@ -2,15 +2,15 @@ void up_down(Servo myservo){
 
   int currentpos=myservo.read();
 
-  if (currentpos==90){
-  for(pos=90;pos<=180;pos+=5) // goes from 0 degrees to 180 degrees 
+  if (currentpos==45){
+  for(pos=45;pos<=90;pos+=5) // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
   }
   } 
-  if(currentpos==180) {
-  for(pos = 180; pos>=90; pos-=5) // goes from 180 degrees to 0 degrees 
+  if(currentpos==90) {
+  for(pos = 90; pos>=45; pos-=5) // goes from 180 degrees to 0 degrees 
   {                                  // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
